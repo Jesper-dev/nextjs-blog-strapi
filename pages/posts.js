4e51b4f90be2413f6533555023cca694418a35b8
JesperKYH <jesper.pettersson@student.kyh.se>
2021-05-18 11:25:34 +0200

    Added post function

--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "../styles/posts.module.scss";
+
 export default function posts() {
-    return (
-        <h1>Posts page here</h1>
-    )
+  const [posts, setPosts] = useState([]);
+
+  useEffect(async () => {
+    const result = await axios.get("http://www.localhost:1337/posts");
+    setPosts(result?.data);
+  }, []);
+  console.log(posts);
+  return (
+    <div className={styles.wrapper}>
+      {posts.map((item) => {
+        return (
+          <div key={item.id} className={styles.postContainer}>
+            <h1>{item.postTitle}</h1>
+            {item.postImg.length > 0 ? (
+              <img src={"http://www.localhost:1337" + item.postImg[0].url} />
+            ) : null}
+            <p>{item.postBody}</p>
+            <span>{item.postDate}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
