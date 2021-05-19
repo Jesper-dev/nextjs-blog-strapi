b5e93602aab90f59689a2db48579af2585b7a393
JesperKYH <jesper.pettersson@student.kyh.se>
2021-05-19 15:10:19 +0200

    added favicon and more mobile styling

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
 import { fetchAPI } from "../lib/api";
+import styles from "../styles/about.module.scss";
 
 export default function posts({ strapiData }) {
-  return <h1>About page here</h1>;
+  return <p className={styles.text}>{strapiData.aboutMe}</p>;
 }
 
 export async function getStaticProps() {
-  const [strapiData] = await Promise.all([fetchAPI("/home-page")]);
+  const [strapiData] = await Promise.all([fetchAPI("/about")]);
   return {
     props: {
       strapiData,
